9f609707cca44ffdfa5dec8d8d62938b72a716c0
Elias Norrby <elias.norrby@gmail.com>
2020-11-15 14:04:27 +0100

    L90: Spring Boot application.properties

--- spring-di/src/main/java/com/eliasnorrby/springdi/config/PropertyConfig.java
+++ spring-di/src/main/java/com/eliasnorrby/springdi/config/PropertyConfig.java
@@ -2,25 +2,12 @@ package com.eliasnorrby.springdi.config;
 
 import com.eliasnorrby.springdi.examplebeans.FakeDataSource;
 import com.eliasnorrby.springdi.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
 
 @Configuration
-// Either of these will work fine:
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-  @PropertySource("classpath:datasource.properties"),
-  @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-  @Autowired
-  Environment env;
 
   @Value("${guru.username}")
   String username;
@@ -43,7 +30,7 @@ public class PropertyConfig {
   @Bean
   public FakeDataSource fakeDataSource() {
     FakeDataSource fakeDataSource = new FakeDataSource();
-    fakeDataSource.setUsername(env.getProperty("USERNAME"));
+    fakeDataSource.setUsername(username);
     fakeDataSource.setPassword(password);
     fakeDataSource.setUrl(url);
     return fakeDataSource;
