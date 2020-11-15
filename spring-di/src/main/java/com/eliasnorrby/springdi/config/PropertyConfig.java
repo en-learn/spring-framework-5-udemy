2b470df4250df2fec29832e725e39e895a295215
Elias Norrby <elias.norrby@gmail.com>
2020-11-15 13:50:55 +0100

    L88: Spring Environment Properties

--- spring-di/src/main/java/com/eliasnorrby/springdi/config/PropertyConfig.java
+++ spring-di/src/main/java/com/eliasnorrby/springdi/config/PropertyConfig.java
@@ -1,15 +1,19 @@
 package com.eliasnorrby.springdi.config;
 
 import com.eliasnorrby.springdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+  @Autowired
+  Environment env;
 
   @Value("${guru.username}")
   String user;
@@ -23,19 +27,9 @@ public class PropertyConfig {
   @Bean
   public FakeDataSource fakeDataSource() {
     FakeDataSource fakeDataSource = new FakeDataSource();
-    fakeDataSource.setUser(user);
+    fakeDataSource.setUser(env.getProperty("USERNAME"));
     fakeDataSource.setPassword(password);
     fakeDataSource.setUrl(url);
     return fakeDataSource;
   }
-
-  // This is not needed any more?
-  //  @Bean
-  //  PropertySourcesPlaceholderConfigurer properties() {
-  //    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
-  //        new PropertySourcesPlaceholderConfigurer();
-  //    System.out.println(
-  //        "user = " + user);
-  //    return propertySourcesPlaceholderConfigurer;
-  //  }
 }
